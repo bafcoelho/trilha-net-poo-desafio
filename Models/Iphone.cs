9a6fe0bd2a1ff9383b95c0c9fc928a0d57b30250
Bruno Coelho <bafcoelho@gmail.com>
2024-10-01 20:49:24 -0300

    feat: implementado desafio trinha .net poo

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Installing App: {nomeApp}");
+            Console.WriteLine($"Validating App Store Face ID...");
+            Console.WriteLine($"{nomeApp} instalado com sucesso!");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("155555-7891", "N95", "12345678910", 4);
+Console.WriteLine(nokia.Numero);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Photo Gallery");
+
+Console.WriteLine("------------------------------");
+
+Iphone iphone = new Iphone("155555-9999", "15 Plus 250g", "12345678888", 8);
+Console.WriteLine(iphone.Numero);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("iMove");
